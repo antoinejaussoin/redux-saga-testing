17ed3e4d1546fc2ec30127a1398d062ad0a150dd
Antoine Jaussoin <antoine@jaussoin.com>
2022-02-12 11:52:02 +0000

    Fix ava tests

--- ava/05.saga-with-input.test.ts
+++ ava/05.saga-with-input.test.ts
@@ -1,6 +1,6 @@
-import sagaHelper from '../helper';
-import { put } from 'redux-saga/effects';
-import avaTest, { Assertions } from 'ava';
+import sagaHelper from "../helper";
+import { put } from "redux-saga/effects";
+import avaTest, { Assertions } from "ava";
 
 interface Action {
   type: string;
@@ -8,23 +8,23 @@ interface Action {
 }
 
 const someOtherAction = (payload: string) => ({
-  type: 'SOME_OTHER_ACTION',
-  payload
+  type: "SOME_OTHER_ACTION",
+  payload,
 });
 
-function* mySaga(action: Action) {
+function* mySaga(action: Action): any {
   yield put(someOtherAction(action.payload));
 }
 
 const test = sagaHelper(
-  mySaga({ type: 'SOME_ACTION', payload: 'foo' }),
+  mySaga({ type: "SOME_ACTION", payload: "foo" }),
   avaTest
 );
 
-test('should trigger the other action with the input action payload', (result, t: Assertions) => {
-  t.deepEqual(result, put(someOtherAction('foo')));
+test("should trigger the other action with the input action payload", (result, t: Assertions) => {
+  t.deepEqual(result, put(someOtherAction("foo")));
 });
 
-test('and then nothing', (result, t: Assertions) => {
+test("and then nothing", (result, t: Assertions) => {
   t.is(result, undefined);
 });
--- ava/10.all-in-one-example.test.ts
+++ ava/10.all-in-one-example.test.ts
@@ -1,7 +1,7 @@
-import sagaHelper from '../helper';
-import { call, put, select } from 'redux-saga/effects';
-import avaTest, { Assertions } from 'ava';
-import sinon from 'sinon';
+import sagaHelper from "../helper";
+import { call, put, select } from "redux-saga/effects";
+import avaTest, { Assertions } from "ava";
+import sinon from "sinon";
 
 interface State {
   filters: string[];
@@ -9,17 +9,17 @@ interface State {
 
 const splitApi = sinon.spy();
 const someActionSuccess = (payload: string[]) => ({
-  type: 'SOME_ACTION_SUCCESS',
-  payload
+  type: "SOME_ACTION_SUCCESS",
+  payload,
 });
-const someActionEmpty = () => ({ type: 'SOME_ACTION_EMPTY' });
+const someActionEmpty = () => ({ type: "SOME_ACTION_EMPTY" });
 const someActionError = (error: string) => ({
-  type: 'SOME_ACTION_ERROR',
-  payload: error
+  type: "SOME_ACTION_ERROR",
+  payload: error,
 });
 const selectFilters = (state: State) => state.filters;
 
-function* mySaga(input: string) {
+function* mySaga(input: string): any {
   try {
     // We get the filters list from the state, using "select"
     const filters = yield select(selectFilters);
@@ -39,81 +39,81 @@ function* mySaga(input: string) {
     } else {
       yield put(someActionSuccess(transformedData));
     }
-  } catch (e) {
+  } catch (e: any) {
     // If we got an exception along the way, we call the error action with the error message
     yield put(someActionError(e.message));
   }
 }
 
 // Scenario 1: When the input contains other words than foo and bar and doesn\'t throw
-let test = sagaHelper(mySaga('hello,foo,bar,world'), avaTest);
+let test = sagaHelper(mySaga("hello,foo,bar,world"), avaTest);
 
-test('Scenario 1: should get the list of filters from the state', (result, t: Assertions) => {
+test("Scenario 1: should get the list of filters from the state", (result, t: Assertions) => {
   t.deepEqual(result, select(selectFilters));
 
   // Here we specify what the selector should have returned.
   // The selector is not called so we have to give its expected return value.
-  return ['foo', 'bar'];
+  return ["foo", "bar"];
 });
 
-test('Scenario 1: should have called the mock API first, which we are going to specify the results of', (result, t: Assertions) => {
-  t.deepEqual(result, call(splitApi, 'hello,foo,bar,world'));
+test("Scenario 1: should have called the mock API first, which we are going to specify the results of", (result, t: Assertions) => {
+  t.deepEqual(result, call(splitApi, "hello,foo,bar,world"));
 
   // Here we specify what the API should have returned.
   // Again, the API is not called so we have to give its expected response.
-  return ['hello', 'foo', 'bar', 'world'];
+  return ["hello", "foo", "bar", "world"];
 });
 
-test('Scenario 1: and then trigger an action with the transformed data we got from the API', (result, t: Assertions) => {
-  t.deepEqual(result, put(someActionSuccess(['hello', 'world'])));
+test("Scenario 1: and then trigger an action with the transformed data we got from the API", (result, t: Assertions) => {
+  t.deepEqual(result, put(someActionSuccess(["hello", "world"])));
 });
 
-test('Scenario 1: and then nothing', (result, t: Assertions) => {
+test("Scenario 1: and then nothing", (result, t: Assertions) => {
   t.is(result, undefined);
 });
 
 // Scenario 2: When the input only contains foo and bar
-test = sagaHelper(mySaga('foo,bar'), avaTest);
+test = sagaHelper(mySaga("foo,bar"), avaTest);
 
-test('Scenario 2: should get the list of filters from the state', (result, t: Assertions) => {
+test("Scenario 2: should get the list of filters from the state", (result, t: Assertions) => {
   t.deepEqual(result, select(selectFilters));
-  return ['foo', 'bar'];
+  return ["foo", "bar"];
 });
 
-test('Scenario 2: should have called the mock API first, which we are going to specify the results of', (result, t: Assertions) => {
-  t.deepEqual(result, call(splitApi, 'foo,bar'));
-  return ['foo', 'bar'];
+test("Scenario 2: should have called the mock API first, which we are going to specify the results of", (result, t: Assertions) => {
+  t.deepEqual(result, call(splitApi, "foo,bar"));
+  return ["foo", "bar"];
 });
 
-test('Scenario 2: and then trigger the empty action since foo and bar are filtered out', (result, t: Assertions) => {
+test("Scenario 2: and then trigger the empty action since foo and bar are filtered out", (result, t: Assertions) => {
   t.deepEqual(result, put(someActionEmpty()));
 });
 
-test('Scenario 2: and then nothing', (result, t: Assertions) => {
+test("Scenario 2: and then nothing", (result, t: Assertions) => {
   t.is(result, undefined);
 });
 
 // Scenario 3: The API is broken and throws an exception
-test = sagaHelper(mySaga('hello,foo,bar,world'), avaTest);
+test = sagaHelper(mySaga("hello,foo,bar,world"), avaTest);
 
-test('Scenario 3: should get the list of filters from the state', (result, t: Assertions) => {
+test("Scenario 3: should get the list of filters from the state", (result, t: Assertions) => {
   t.deepEqual(result, select(selectFilters));
-  return ['foo', 'bar'];
+  return ["foo", "bar"];
 });
 
-test('Scenario 3: should have called the mock API first, which will throw an exception', (result, t: Assertions) => {
-  t.deepEqual(result, call(splitApi, 'hello,foo,bar,world'));
+test("Scenario 3: should have called the mock API first, which will throw an exception", (result, t: Assertions) => {
+  t.deepEqual(result, call(splitApi, "hello,foo,bar,world"));
 
   // Here we pretend that the API threw an exception.
   // We don't "throw" here but we return an error, which will be considered by the
   // redux-saga-testing helper to be an exception to throw on the generator
-  return new Error('Something went wrong');
+  return new Error("Something went wrong");
 });
 
-test('Scenario 3: and then trigger an error action with the error message', (result, t: Assertions) => {
-  t.deepEqual(result, put(someActionError('Something went wrong')));
+test("Scenario 3: and then trigger an error action with the error message", (result, t: Assertions) => {
+  t.deepEqual(result, put(someActionError("Something went wrong")));
 });
 
-test('Scenario 3: and then nothing', (result, t: Assertions) => {
+test("Scenario 3: and then nothing", (result, t: Assertions) => {
   t.is(result, undefined);
 });
